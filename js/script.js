46be11c5bc0137629c7aefb0bc19b2a5ad6ef8f7
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-09 15:40:27 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
     //DROPDOWN
     //Refs
-    var dropLink = $(".with-dropdown a");
-    var dropElement = $(".dropdown");
+    var dropDown = $(".with-dropdown");
+    var dropLink = dropDown.children("a");
+    var dropElement = dropDown.children(".dropdown")
 
     dropLink.click(function() {
-        
+        var openMenu = $(this).next(dropElement);
+        //console.log(openMenu);
+        // Reset
+        dropElement.not(openMenu).hide();
+
+        openMenu.toggle();
     });
 
 
