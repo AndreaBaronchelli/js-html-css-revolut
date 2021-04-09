7c12a8e83812482fc2306be32a56cb024e50cc07
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-09 14:49:28 +0200

    add html css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
+    //DROPDOWN
+    //Refs
+    var dropLink = $(".with-dropdown a");
+    var dropElement = $(".dropdown");
 
-    
-
+    dropLink.click(function() {
+        
+    });
 
 
 //End doc ready
